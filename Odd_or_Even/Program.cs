26da84b8b3679da26665502f980fe07b36f8d785
martinmp <martomsl@gmail.com>
2023-08-07 15:58:04 +0300

    task 1 solution

--- Odd_or_Even/Program.cs
+++ Odd_or_Even/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> num = null;
+            string s = Console.ReadLine();
+
+            try {
+                num = int.Parse(s);
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please enter a valid integer!");
+                return;
+            }
+
+            if (num % 2 == 0) {
+                Console.WriteLine("Even");
+            }
+            else 
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
