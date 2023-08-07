12651518bd5bfaecd50ad160d4a45f27616acce1
martinmp <martomsl@gmail.com>
2023-08-07 16:36:29 +0300

    task 2 solution

---
+++ Moon_Gravity/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyProject
+{
+
+    class Program
+    {
+        public static float CaluclateMoonWeight(float w)
+        {
+            float moonGravity = 0.17f;
+            return w * moonGravity;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0:0.000}", Program.CaluclateMoonWeight(75.4567f));
+        }
+    }
+}
