acd10356d2790020cdf9a57c6b37274735504834
martinmp <martomsl@gmail.com>
2023-08-08 15:05:24 +0300

    task 11

---
+++ 3rd_Bit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            // BitArray b = new BitArray(new int[] { num }); 
+
+            // Console.WriteLine(b[2] ? 1 : 0);
+            string bitNumToString = Convert.ToString(num, 2);
+
+            Console.WriteLine(bitNumToString[2]);
+        }
+    }
+}
