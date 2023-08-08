b5706423d491770dcfa4b42861ce4a68526b676c
martinmp <martomsl@gmail.com>
2023-08-08 13:38:46 +0300

    tastk 8

---
+++ Prime_check/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        public static bool IsPrime(int n)
+        {
+            int countDevisions = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    countDevisions++;
+                }
+            }
+
+            if (countDevisions == 2)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            int num = int.Parse(input);
+
+            Console.WriteLine(IsPrime(num));
+        }
+    }
+}
