48ea37dc1cea3ae0b660b1f3840702e069cda18e
martinmp <martomsl@gmail.com>
2023-08-08 12:00:30 +0300

    task 7

---
+++ Four_digits/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyProject
+{
+
+    class Program
+    {
+        public static double SumDigits(string str)
+        {
+            return Convert.ToInt32(Char.GetNumericValue(str[0]) + Char.GetNumericValue(str[1]) + Char.GetNumericValue(str[2]) + Char.GetNumericValue(str[3]));
+        }
+
+        public static string ReverseDigits(string str)
+        {
+            return Char.ToString(str[3]) + Char.ToString(str[2]) + Char.ToString(str[1]) + Char.ToString(str[0]);
+        }
+
+        public static string LastToFirst(string str)
+        {
+            return Char.ToString(str[3]) + Char.ToString(str[0]) + Char.ToString(str[1]) + Char.ToString(str[2]);
+        }
+
+        public static string SecondToThird(string str)
+        {
+            return Char.ToString(str[0]) + Char.ToString(str[2]) + Char.ToString(str[1]) + Char.ToString(str[3]);
+        }
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(SumDigits(input));
+            Console.WriteLine(ReverseDigits(input));
+            Console.WriteLine(LastToFirst(input));
+            Console.WriteLine(SecondToThird(input));
+        }
+    }
+}
