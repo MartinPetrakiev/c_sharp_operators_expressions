f4582fa19e62028b9a00f51b489800332543a6e7
martinmp <martomsl@gmail.com>
2023-08-08 13:53:46 +0300

    task 9

---
+++ Trapezoids/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        public static float TrapezoidArea(float a, float b, float h)
+        {
+            return (a + b) * h / 2;
+        }
+        static void Main(string[] args)
+        {
+            var input1 = Console.ReadLine();
+            var input2 = Console.ReadLine();
+            var input3 = Console.ReadLine();
+            float a = float.Parse(input1);
+            float b = float.Parse(input2);
+            float h = float.Parse(input3);
+ 
+            Console.WriteLine(TrapezoidArea(a, b, h).ToString("n7"));  
+        }
+    }
+}
